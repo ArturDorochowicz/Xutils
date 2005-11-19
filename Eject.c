6b3318a2b50f56091b74d5a31334622db2bd7a5a
Artur Dorochowicz <artur@dorochowicz.com>
2005-11-19 22:58:54 +0000

    Wysuwania i ładowanie ograniczone do stacji CD. Obie operacje są wymuszane.

--- Eject.c
+++ Eject.c
@@ -2,175 +2,60 @@
 #include "Xutils.h"
 
 
-HANDLE OpenVolume(TCHAR cDriveLetter);
-BOOL LockVolume(HANDLE hVolume);
-BOOL DismountVolume(HANDLE hVolume);
-BOOL PreventRemovalOfVolume(HANDLE hVolume, BOOL fPrevent);
-BOOL AutoEjectVolume(HANDLE hVolume);
-BOOL CloseVolume(HANDLE hVolume);
-
-LPTSTR szVolumeFormat = TEXT("\\\\.\\%c:");
-LPTSTR szRootFormat = TEXT("%c:\\");
-
-HANDLE OpenVolume( char driveLetter )
+HANDLE OpenCdVolume( char driveLetter )
 {
-	HANDLE hVolume;
-	UINT uDriveType;
-	char szVolumeName[8];
-	char szRootName[5];
-	DWORD dwAccessFlags;
+	static char volumeFormat [] = "\\\\.\\%c:";
+	static char rootFormat [] = "%c:\\";
 
-	sprintf( szRootName, szRootFormat, driveLetter );
+	HANDLE volume = INVALID_HANDLE_VALUE;
+	char volumeName [8];
+	char rootName [5];
 
-	uDriveType = GetDriveType( szRootName );
-	switch( uDriveType )
+	sprintf( rootName, rootFormat, driveLetter );
+
+	if( DRIVE_CDROM == GetDriveType( rootName ) )
 	{
-	   case DRIVE_REMOVABLE:
-		   dwAccessFlags = GENERIC_READ | GENERIC_WRITE;
-		   break;
-
-	   case DRIVE_CDROM:
-		   dwAccessFlags = GENERIC_READ;
-		   break;
-
-	   default:
-		   ShowErrorMessage( "Cannot eject. Drive type is incorrect." );
-		   return INVALID_HANDLE_VALUE;
+		sprintf( volumeName, volumeFormat, driveLetter );
+		volume = CreateFile( volumeName, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL );
 	}
 
-	sprintf( szVolumeName, szVolumeFormat, driveLetter );
-
-	if( INVALID_HANDLE_VALUE == ( hVolume = CreateFile( szVolumeName, dwAccessFlags,
-		FILE_SHARE_READ | FILE_SHARE_WRITE,
-		NULL, OPEN_EXISTING, 0, NULL ) ) )
-	{
-		OutputDebugString( "CreateFile error." );
-	}
-
-	return hVolume;
-}
-
-BOOL CloseVolume( HANDLE volume )
-{
-	return CloseHandle( volume );
-}
-
-#define LOCK_TIMEOUT 10000       // 10 Seconds
-#define LOCK_RETRIES 20
-
-BOOL LockVolume( HANDLE hVolume )
-{
-	DWORD dwBytesReturned;
-	DWORD dwSleepAmount;
-	int nTryCount;
-
-	dwSleepAmount = LOCK_TIMEOUT / LOCK_RETRIES;
-
-	// Do this in a loop until a timeout period has expired
-	for( nTryCount = 0; nTryCount < LOCK_RETRIES; ++nTryCount )
-	{
-		if( DeviceIoControl( hVolume,
-			FSCTL_LOCK_VOLUME,
-			NULL, 0,
-			NULL, 0,
-			&dwBytesReturned,
-			NULL ) )
-			return TRUE;
-
-		Sleep( dwSleepAmount );
-	}
-
-	return FALSE;
-}
-
-BOOL DismountVolume( HANDLE hVolume )
-{
-	DWORD dwBytesReturned;
-
-	return DeviceIoControl( hVolume,
-		FSCTL_DISMOUNT_VOLUME,
-		NULL, 0,
-		NULL, 0,
-		&dwBytesReturned,
-		NULL );
-}
-
-BOOL PreventRemovalOfVolume( HANDLE hVolume, BOOL fPreventRemoval )
-{
-	DWORD dwBytesReturned;
-	PREVENT_MEDIA_REMOVAL PMRBuffer;
-
-	PMRBuffer.PreventMediaRemoval = (BOOLEAN) fPreventRemoval;
-
-	return DeviceIoControl( hVolume,
-		IOCTL_STORAGE_MEDIA_REMOVAL,
-		&PMRBuffer, sizeof( PREVENT_MEDIA_REMOVAL ),
-		NULL, 0,
-		&dwBytesReturned,
-		NULL );
-}
-
-BOOL AutoEjectVolume( HANDLE hVolume )
-{
-	DWORD dwBytesReturned;
-
-	return DeviceIoControl( hVolume, 
-		IOCTL_STORAGE_EJECT_MEDIA,
-		NULL, 0,
-		NULL, 0,
-		&dwBytesReturned,
-		NULL );
+	return volume;
 }
 
 BOOL EjectCd( char driveLetter )
 {
-	HANDLE hVolume;
-	BOOL fRemoveSafely = FALSE;
-	BOOL fAutoEject = FALSE;
+	BOOL isOk = FALSE;
+	HANDLE volume;
+	DWORD bytesReturned;
 
 	// Open the volume.
-	hVolume = OpenVolume( driveLetter );
-	if( hVolume == INVALID_HANDLE_VALUE )
-		return FALSE;
-
-	// Lock and dismount the volume.
-	if( LockVolume( hVolume ) && DismountVolume( hVolume ) )
+	if( INVALID_HANDLE_VALUE != ( volume = OpenCdVolume( driveLetter ) ) )
 	{
-		fRemoveSafely = TRUE;
+		if( DeviceIoControl( volume, IOCTL_STORAGE_EJECT_MEDIA, NULL, 0, NULL, 0, &bytesReturned, NULL ) )
+			isOk = TRUE;
 
-		// Set prevent removal to false and eject the volume.
-		if( PreventRemovalOfVolume( hVolume, FALSE) && AutoEjectVolume( hVolume ) )
-			fAutoEject = TRUE;
+		// Close the volume so other processes can use the drive.
+		if( !CloseHandle( volume ) )
+			isOk = FALSE;
 	}
 
-	// Close the volume so other processes can use the drive.
-	if( !CloseVolume( hVolume ) )
-		return FALSE;
-
-	if( fAutoEject )
-		printf( "Media in Drive %c has been ejected safely.\n", driveLetter );
-	else if( fRemoveSafely )
-		printf( "Media in Drive %c can be safely removed.\n", driveLetter );
-
-	return TRUE;
+	return isOk;
 }
 
 BOOL LoadCd( char driveLetter )
 {
 	BOOL isOk = FALSE;
 	HANDLE volume;
-	DWORD bytesRet;
+	DWORD bytesReturned;
 	
 	// Open the volume.	
-	if( INVALID_HANDLE_VALUE != ( volume = OpenVolume( driveLetter ) ) )
+	if( INVALID_HANDLE_VALUE != ( volume = OpenCdVolume( driveLetter ) ) )
 	{
-		if( DeviceIoControl( volume, IOCTL_STORAGE_LOAD_MEDIA, NULL, 0, NULL, 0, &bytesRet, NULL ) )
-		{
+		if( DeviceIoControl( volume, IOCTL_STORAGE_LOAD_MEDIA, NULL, 0, NULL, 0, &bytesReturned, NULL ) )
 			isOk = TRUE;
-		}
 	
 		// Close the volume so other processes can use the drive.
-		if( !CloseVolume( volume ) )
+		if( !CloseHandle( volume ) )
 			isOk = FALSE;
 	}
 
@@ -207,14 +92,14 @@ _declspec( dllexport ) void loadcd( PSTR szv, PSTR szx, BOOL (*GetVar)(PSTR, PST
 
 int main( int argc, char **argv )
 {	
-	OutputDebugString( "tttdasdasdasd" );
+	//OutputDebugString( "tttdasdasdasd" );
 	//SuDo( L"c:\\windows\\system32\\cmd.exe", L"", L"" );
 
-	EjectCd( 'x' );
+	EjectCd( argv[1][0] );
 
 	Sleep( 3000 );
 
-	LoadCd( 'x' );
+	LoadCd( argv[1][0] );
 
 	return 0;
 }
