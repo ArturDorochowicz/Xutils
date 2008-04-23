b5b3f8e489b1f6e9151c67a98bde20d31bbd9c56
Artur Dorochowicz <artur@dorochowicz.com>
2008-04-23 17:42:16 +0000

    First step towards newer project structure - some files deleted, some moved.

---
+++ src/Xutils.c
@@ -0,0 +1,180 @@
+/**
+ * Xutils
+ * Copyright (C) Artur Dorochowicz
+ *
+ * Released under the terms of Lesser General Public License (LGPL).
+ *
+ */
+
+
+#include "Xutils.h"
+
+/*---------------------------------------------------------------------------*/
+
+PowerProServices * PPServices = NULL;
+
+/**
+ * Library's main entry point, no special processing needed
+ */
+BOOL WINAPI DllMain( HINSTANCE hinstDLL, DWORD dwReason, PVOID lpvReserved )
+{
+	return TRUE;
+}
+
+void ShowErrorMessage( const char * message )
+{
+	if( PPServices != NULL )
+	{
+		PPServices->ErrMessage( (char*) message, NULL );
+	}
+	else
+	{
+		MessageBox( 0, message, "Error", MB_OK | MB_ICONERROR );
+	}
+}
+
+void ShowLastError( )
+{
+	char * messageBuffer;
+
+	FormatMessage( FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL,
+		GetLastError( ),
+		MAKELANGID( LANG_NEUTRAL, SUBLANG_DEFAULT ),
+		(char*) &messageBuffer,
+		0, NULL );
+
+	ShowErrorMessage( messageBuffer );
+	LocalFree( messageBuffer );
+}
+
+BOOL CheckArgumentsCount( Services service, int nArgs )
+{
+	BOOL nArgsOk = TRUE;
+	
+	static const char ARGS_MAX_1 [] = "The service needs at most one argument.";
+	static const char ARGS_MAX_3 [] = "The service needs at most three arguments.";
+	static const char ARGS_MAX_4 [] = "The service needs at most four arguments.";
+
+	static const char ARGS_MIN_1 [] = "The service needs at least one argument.";
+
+	switch( service )
+	{
+		case ServiceSudo:
+		{
+			if( nArgs > 3 )
+			{
+				nArgsOk = FALSE;
+				ShowErrorMessage( ARGS_MAX_3 );
+			}
+			else if( nArgs < 1 )
+			{
+				nArgsOk = FALSE;
+				ShowErrorMessage( ARGS_MIN_1 );
+			}
+			break;
+		}
+
+		case ServiceRunas:
+		{
+			if( nArgs > 4 )
+			{
+				nArgsOk = FALSE;
+				ShowErrorMessage( ARGS_MAX_4 );
+			}
+			else if( nArgs < 1 )
+			{
+				nArgsOk = FALSE;
+				ShowErrorMessage( ARGS_MIN_1 );
+			}
+			break;
+		}
+
+		case ServiceEjectcd:
+		{
+			if( nArgs > 1 )
+			{
+				nArgsOk = FALSE;
+				ShowErrorMessage( ARGS_MAX_1 );
+			}
+			break;
+		}
+
+		case ServiceLoadcd:
+		{
+			if( nArgs > 1 )
+			{
+				nArgsOk = FALSE;
+				ShowErrorMessage( ARGS_MAX_1 );
+			}
+			break;
+		}
+
+		case ServiceDisableidletimers:
+		{
+			if( nArgs > 1 )
+			{
+				nArgsOk = FALSE;
+				ShowErrorMessage( ARGS_MAX_1 );
+			}
+			break;
+		}
+
+		case ServiceEnableidletimers:
+		{
+			if( nArgs > 1 )
+			{
+				nArgsOk = FALSE;
+				ShowErrorMessage( ARGS_MAX_1 );
+			}
+			break;
+		}
+
+		case ServiceScrollup:
+		{
+			if( nArgs > 1 )
+			{
+				nArgsOk = FALSE;
+				ShowErrorMessage( ARGS_MAX_1 );
+			}
+			break;
+		}
+
+		case ServiceScrolldown:
+		{
+			if( nArgs > 1 )
+			{
+				nArgsOk = FALSE;
+				ShowErrorMessage( ARGS_MAX_1 );
+			}
+			break;
+		}
+
+	}
+
+	return nArgsOk;
+}
+
+BOOL ConvertMultiByteToWideChar( const char * ansiStr, wchar_t ** wideStr )
+{
+	BOOL isOk = FALSE;
+	int wideCharsNeeded = MultiByteToWideChar( CP_THREAD_ACP, MB_ERR_INVALID_CHARS, ansiStr, -1, NULL, 0 );
+
+	if( wideCharsNeeded > 0 )
+	{		
+		if( NULL != ( *wideStr = malloc( wideCharsNeeded * sizeof( wchar_t ) ) ) )
+		{
+			if( 0 != MultiByteToWideChar( CP_THREAD_ACP, MB_ERR_INVALID_CHARS, ansiStr, -1, *wideStr, wideCharsNeeded ) )
+			{
+				isOk = TRUE;
+			}
+			else
+			{
+				free( wideStr );
+				*wideStr = NULL;
+			}
+		}
+	}
+
+	return isOk;
+}
---
+++ src/Xutils.h
@@ -0,0 +1,88 @@
+/**
+ * Xutils
+ * Copyright (C) Artur Dorochowicz
+ *
+ * Released under the terms of Lesser General Public License (LGPL).
+ *
+ */
+
+
+#ifndef XUTILS_H
+#define XUTILS_H
+
+#include <stdio.h>
+
+// need Windows XP and later APIs
+#define _WIN32_WINNT 0x0501
+#include <Windows.h>
+
+
+typedef enum tagServices
+{
+	ServiceRunas,
+	ServiceSudo,
+	ServiceEjectcd,
+	ServiceLoadcd,
+	ServiceDisableidletimers,
+	ServiceEnableidletimers,
+	ServiceScrollup,
+	ServiceScrolldown
+} Services;
+
+typedef struct tagPowerProServices
+{
+	void (*ErrMessage)(PSTR, PSTR);
+	BOOL (*MatchCaption)(HWND, PSTR);
+	HWND (*FindMatchingWindow)(PSTR,BOOL);
+	BOOL (*IsRolled)(HWND hw);
+	BOOL (*IsTrayMinned)(HWND hw);
+	void (*GetExeFullPath)(HWND hw, PSTR szt);
+	void (*RollUp)(HWND hw);
+	void (*TrayMin)(HWND hw);
+	void (*SendKeys)(PSTR sz);
+	BOOL (*EvalExpr)(PSTR sz, PSTR szo);
+	void (*Debug)(PSTR sz1, PSTR sz2,PSTR sz3, PSTR sz4, PSTR sz5, PSTR sz6);
+	PSTR (*AllocTemp)(UINT leng);
+	void (*ReturnString)(PSTR sz, PSTR* szargs);
+	PSTR (*GetVarAddr)(PSTR var);
+	PSTR (*SetVar)(PSTR var, PSTR val);
+	void (*IgnoreNextClip)();
+	void (*Show)(HWND h);
+	void (*RunCmd)(PSTR szCmd, PSTR szParam, PSTR szWork);
+	BOOL (*InsertStringForBar)( PSTR szStr, PSTR szCmd);
+	void (*ResetFocus)();
+	HWND (*NoteOpen)(PSTR szFile, PSTR szKeyWords);
+	BOOL (*PumpMessages)();
+	BOOL (*RegForConfig)(void ( *callback )(PSTR szList), BOOL bReg );
+	void (*SetPreviousFocus)(HWND h );
+	UINT (*SetDebug)(PSTR sz );
+	UINT (*ScriptCancel)(PSTR sz );
+	void (*GetCurrentDir)(HWND h,PSTR szt);
+} PowerProServices;
+
+extern PowerProServices * PPServices;
+
+/**
+ * Display error message.
+ * Message is displayed with PPServices->ErrMessage
+ * or an ordinary MessageBox, when PPServices is NULL.
+ */
+void ShowErrorMessage( const char * message );
+
+/**
+ * Format and display error message for error code obtained from GetLastError().
+ */
+void ShowLastError( );
+
+/**
+ * Check number of arguments.
+ */
+BOOL CheckArgumentsCount( Services service, int nArgs );
+
+/**
+ * Convert specified ANSI string to Unicode string.
+ * Caller is responsible for freeing allocated wide string.
+ */
+BOOL ConvertMultiByteToWideChar( const char * ansiStr, wchar_t ** wideStr );
+
+#endif   // #ifndef XUTILS_H
